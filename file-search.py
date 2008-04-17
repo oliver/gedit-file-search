a19104572e0cbae81edbabb105bc47b63fcfe473
Oliver Gerlich <oliver.gerlich@gmx.de>
2008-04-17 20:56:35 +0200

    add Search -> "Find in files" menu item

--- file-search.py
+++ file-search.py
@@ -1,11 +1,25 @@
 
 import gedit
+import gtk
+
+ui_str = """<ui>
+  <menubar name="MenuBar">
+    <menu name="SearchMenu" action="Search">
+      <placeholder name="SearchOps_2">
+        <menuitem name="FileSearch" action="FileSearch"/>
+      </placeholder>
+    </menu>
+  </menubar>
+</ui>
+"""
 
 class FileSearchWindowHelper:
     def __init__(self, plugin, window):
         print "Plugin created for", window
         self._window = window
         self._plugin = plugin
+
+        self._insert_menu()
 
     def deactivate(self):
         print "Plugin stopped for", self._window
@@ -16,6 +30,26 @@ class FileSearchWindowHelper:
         # Called whenever the window has been updated (active tab
         # changed, etc.)
         print "Plugin update for", self._window
+
+    def _insert_menu(self):
+        # Get the GtkUIManager
+        manager = self._window.get_ui_manager()
+
+        # Create a new action group
+        self._action_group = gtk.ActionGroup("FileSearchPluginActions")
+        self._action_group.add_actions([("FileSearch", "gtk-find", _("Find in files ..."),
+                                         None, _("Search in multiple files"),
+                                         self.on_search_files_activate)])
+
+        # Insert the action group
+        manager.insert_action_group(self._action_group, -1)
+
+        # Merge the UI
+        self._ui_id = manager.add_ui_from_string(ui_str)
+
+    def on_search_files_activate(self, action):
+        print "(find in files)"
+
 
 class FileSearchPlugin(gedit.Plugin):
     def __init__(self):
